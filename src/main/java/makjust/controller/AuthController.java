8a34bbf4a37389e8dcd88be43528a5942855898d
Crazydouzi <2715170004@qq.com>
2022-11-10 22:20:01 +0800

    添加注释

--- src/main/java/makjust/controller/AuthController.java
+++ src/main/java/makjust/controller/AuthController.java
@@ -10,19 +10,23 @@ import makjust.pojo.User;
 
 @Controller("/core")
 public class AuthController {
+    // 用户登录
     @RequestMapping(value = "/userLogin",method = HttpMethod.POST)
     public JsonObject userLogin(@RequestBody  User user, RoutingContext ctx) {
         return JsonObject.mapFrom(user);
     }
+    // 用户注册
     @RequestMapping(value = "/userRegister",method = HttpMethod.POST)
     public JsonObject userRegister(@RequestBody  User user) {
         return JsonObject.mapFrom(user);
     }
+    // 登出
     @RequestMapping(value = "/userLogout",method = HttpMethod.POST)
     public JsonObject userLogout(@RequestBody  User user){
         return JsonObject.mapFrom(user);
 
     }
+    // 远程认证
     public JsonObject RemoteAuth(@RequestBody  User user){
         return new JsonObject();
     }
