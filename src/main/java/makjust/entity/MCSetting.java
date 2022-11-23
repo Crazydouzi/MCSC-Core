811751ee4f0bbecf72f51b1111b58f655a6660b8
Crazydouzi <2715170004@qq.com>
2022-11-23 14:50:31 +0800

    add: class 'MCSetting' add 'serverId'

--- src/main/java/makjust/entity/MCSetting.java
+++ src/main/java/makjust/entity/MCSetting.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class MCSetting {
     private Integer id;
+    private Integer serverId;
     private String option;
     private String value;
     private Boolean enable;
