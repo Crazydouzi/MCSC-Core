e23c87ed7b3cf336f0eed0013dade0542529e4d9
Crazydouzi <douzi.site@qq.com>
2023-01-19 01:09:26 +0800

    实现serverScanner用于扫描服务器

--- src/main/java/makjust/service/impl/MCVersionServiceImpl.java
+++ src/main/java/makjust/service/impl/MCVersionServiceImpl.java
@@ -1,9 +1,19 @@
 package makjust.service.impl;
 
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.file.FileSystem;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import makjust.service.MCVersionService;
+import makjust.utils.SysConfig;
+
+import java.util.List;
 
 public class MCVersionServiceImpl implements MCVersionService {
+    private final String DIR = SysConfig.getCorePath("/");
 
     @Override
     public JsonObject getVersionList() {
@@ -16,7 +26,19 @@ public class MCVersionServiceImpl implements MCVersionService {
     }
 
     @Override
-    public JsonObject scanVersion() {
-        return null;
+    public void serverScanner(Vertx vertx, Handler<AsyncResult<JsonObject>> resultHandler) {
+        FileSystem fileSystem = vertx.fileSystem();
+        JsonArray jsonArray = new JsonArray();
+        fileSystem.readDir(DIR, "[^.]*").onSuccess(dirList -> {
+            for (String loc : dirList) {
+                JsonObject jsonObject = new JsonObject();
+                List<String> jarList = fileSystem.readDirBlocking(loc, "\\w*.jar");
+                jarList.replaceAll(f -> f.substring(f.lastIndexOf("\\") + 1));
+                jsonObject.put("jarList", jarList);
+                jsonArray.add(new JsonObject().put(loc.substring(loc.lastIndexOf("\\") + 1), jsonObject));
+            }
+            resultHandler.handle(Future.succeededFuture(new JsonObject().put("data", jsonArray)));
+
+        });
     }
 }
