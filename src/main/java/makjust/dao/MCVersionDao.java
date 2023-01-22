605d1c5b13441630edec8db233437850d3cd87b4
Crazydouzi <2715170004@qq.com>
2023-01-22 23:10:45 +0800

    实现查询服务器列表

--- src/main/java/makjust/dao/MCVersionDao.java
+++ src/main/java/makjust/dao/MCVersionDao.java
@@ -1,5 +1,10 @@
 package makjust.dao;
 
+import io.vertx.core.Future;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+
 public interface MCVersionDao {
+    Future<RowSet<Row>> selectMCServerList();
 
 }
--- src/main/java/makjust/dao/impl/MCVersionDaoImpl.java
+++ src/main/java/makjust/dao/impl/MCVersionDaoImpl.java
@@ -7,4 +7,8 @@ import makjust.utils.DBUtils;
 
 public class MCVersionDaoImpl implements makjust.dao.MCVersionDao {
 
+    @Override
+    public Future<RowSet<Row>> selectMCServerList() {
+        return  DBUtils.executeRowSQL("select * from mc_server;");
+    }
 }
