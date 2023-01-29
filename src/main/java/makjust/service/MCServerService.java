37c006cbf1649b5993b07958c780e0000b8229f7
Crazydouzi <2715170004@qq.com>
2023-01-29 15:02:42 +0800

    加点注释

--- src/main/java/makjust/service/MCServerService.java
+++ src/main/java/makjust/service/MCServerService.java
@@ -9,10 +9,16 @@ import makjust.entity.MCServer;
 import java.util.Map;
 
 public interface MCServerService {
+    //核心基本配置设定
     void setCoreSetting(MCServer mcServer,Handler<AsyncResult<JsonObject>> resultHandler);
+    //服务器配置设定
     void setServerSetting(Map<String,Object> optionMap,Handler<AsyncResult<JsonObject>> resultHandler);
+    //获取设置
     void getSetting(Vertx vertx, MCServer mcServer, Handler<AsyncResult<JsonObject>> resultHandler);
+    //开启服务器
     void serverStart(Vertx vertx, Handler<AsyncResult<JsonObject>> resultHandler) ;
+    //关闭服务器
     boolean serverStop(Vertx vertx);
+    //获取可用服务器
     void getEnableServer(Handler<AsyncResult<JsonObject>> resultHandler);
 }
