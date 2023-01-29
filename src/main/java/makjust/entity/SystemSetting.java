fbf3a787ff68fd7afd360ba5c9626060118ef781
Crazydouzi <2715170004@qq.com>
2023-01-29 19:44:23 +0800

    暂时保留

---
+++ src/main/java/makjust/entity/SystemSetting.java
@@ -0,0 +1,15 @@
+package makjust.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+//用于反序列化时下划线转驼峰
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class SystemSetting {
+}
