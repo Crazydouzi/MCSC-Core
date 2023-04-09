2b046bd11e8a7eaf0f9c4074e8201ac19e5ae6ef
Crazydouzi <2715170004@qq.com>
2023-04-09 22:56:57 +0800

    区分Json数据和RequestParam

--- src/main/java/makjust/annotation/RequestParam.java
+++ src/main/java/makjust/annotation/RequestParam.java
@@ -4,6 +4,6 @@ import java.lang.annotation.*;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER})
-public @interface JsonData{
+public @interface RequestParam {
     String value() default "";
 }
